4298c3bd0372bfb85afc3facff382ad55f67a412
Evgeny Duvanov <evgenysimukhin@gmail.com>
2024-02-14 01:01:55 -0300

    футер правка

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,21 +1,22 @@
-<footer class="container p-3 border-top">
-    <div class="text-center">
-        {{-- <h5>ЗАВТРА В ГОРЫ!</h5> --}}
-        <ul class="nav flex-column">
-        <li class="nav-item mb-2"><a href="#price" class="nav-link p-0 text-body-secondary">Прайс</a></li>
-        <li class="nav-item mb-2"><a href="#calculate" class="nav-link p-0 text-body-secondary">Калькулятор</a></li>
-        <li class="nav-item mb-2"><a href="#application" class="nav-link p-0 text-body-secondary">Оставить заявку</a></li>
-        <li class="nav-item mb-2"><a href="#reviews" class="nav-link p-0 text-body-secondary">Отзывы</a></li>
-        <li class="nav-item mb-2"><a href="#faq" class="nav-link p-0 text-body-secondary">FAQs</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">О персональных данных</a></li>
-
-        <li class="text-muted mt-3 mb-md-0 text-body-secondary">
-            Product by 
-            <a href="https://t.me/ea_duv" class="no-decoration">
-                Evgenii Duvanov
+<footer class="navbar-dark bg-dark">
+    <div class="container">
+        <div class="py-3 my-4">
+            <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+                <li class="nav-item mb-2 mx-3"><a href="#price" class="nav-link p-0 text-white">Прайс</a></li>
+                <li class="nav-item mb-2 mx-3"><a href="#calculate" class="nav-link p-0 text-white">Калькулятор</a></li>
+                <li class="nav-item mb-2 mx-3"><a href="#application" class="nav-link p-0 text-white">Оставить заявку</a></li>
+                <li class="nav-item mb-2 mx-3"><a href="#reviews" class="nav-link p-0 text-white">Отзывы</a></li>
+                <li class="nav-item mb-2 mx-3"><a href="#faq" class="nav-link p-0 text-white">FAQs</a></li>
+                <li class="nav-item mb-2 mx-3"><a href="#" class="nav-link p-0 text-white">О персональных данных</a></li>
+            </ul>
+            <a class="navbar-brand" href="#">
+                <img src="{{ url('image/log.png') }}" alt="Логотип" height="90">
             </a>
-        </p>
-
-        </ul>
+            <div class="nav-link p-0 text-white"> Product by
+                <a href="https://t.me/ea_duv" class="no-decoration">
+                    Evgenii Duvanov
+                </a>
+            </div> 
+        </div>
     </div>
 </footer>
