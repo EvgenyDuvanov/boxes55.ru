3e061552d69cfa98bc1a659f278479f0f1da0eeb
Evgeny Duvanov <evgenysimukhin@gmail.com>
2024-06-22 22:26:23 -0300

    яша реклама новый  тест5

--- resources/views/page/yandex.blade.php
+++ resources/views/page/yandex.blade.php
@@ -1,11 +1,10 @@
-<!-- Yandex.RTB R-A-9578878-1 -->
-<div id="yandex_rtb_R-A-9578878-1"></div>
+<!-- Yandex.RTB R-A-9578878-3 -->
+<div id="yandex_rtb_R-A-9578878-3"></div>
 <script>
 window.yaContextCb.push(()=>{
 	Ya.Context.AdvManager.render({
-		"blockId": "R-A-9578878-1",
-		"renderTo": "yandex_rtb_R-A-9578878-1",
-		"type": "feed"
+		"blockId": "R-A-9578878-3",
+		"renderTo": "yandex_rtb_R-A-9578878-3"
 	})
 })
 </script>
