737dd6e3d26bef2505a3aa26952cdedaaf98ee8e
Evgeny Duvanov <evgenysimukhin@gmail.com>
2024-07-29 00:42:45 -0300

    добавлен чат ТГ

--- resources/views/page/contacts.blade.php
+++ resources/views/page/contacts.blade.php
@@ -27,7 +27,7 @@
                             <path d="M14 19h-4a8 8 0 0 1 -8 -8v-5h4v5a4 4 0 0 0 4 4h0v-9h4v4.5l.03 0a4.531 4.531 0 0 0 3.97 -4.496h4l-.342 1.711a6.858 6.858 0 0 1 -3.658 4.789h0a5.34 5.34 0 0 1 3.566 4.111l.434 2.389h0h-4a4.531 4.531 0 0 0 -3.97 -4.496v4.5z"></path>
                         </svg>
                     </a>
-                    <a href="https://t.me/boxes155" class="no-decoration">
+                    <a href="https://t.me/boxes55_chat" class="no-decoration">
                         <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-telegram" width="32" height="32" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                             <path d="M15 10l-4 4l6 6l4 -16l-18 7l4 2l2 6l3 -4" />
